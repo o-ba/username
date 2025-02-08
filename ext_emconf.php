34bd57cf8928fa70ee58d53af0b68900b74863a8
Oli Bartsch <bo@cedev.de>
2025-02-08 12:04:45 +0100

    Update ext_emconf

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,12 +3,12 @@
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Username',
     'description' => 'Login provider to login with username only.',
-    'category' => 'backend',
+    'category' => 'services',
     'author' => 'Oliver Bartsch',
     'author_email' => 'bo@cedev.de',
     'author_company' => '',
-    'state' => 'beta',
-    'version' => '0.1.0',
+    'state' => 'stable',
+    'version' => '0.1.1',
     'constraints' => [
         'depends' => [
             'typo3' => '12.4.0-13.4.99',
